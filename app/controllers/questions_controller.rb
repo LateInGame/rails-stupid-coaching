72c5ae8253b092eccf18a29d53ce8b6725e60a73
David Deus <davidafonsodeus@outlook.com>
2022-11-14 12:24:23 +0000

    done, css not

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
     if params[:question] == 'I am going to work'
       @answer = 'Great!'
     elsif params[:question].include?('?')
-      @answer = 'Silly question, get dressed and go to work!.'
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
